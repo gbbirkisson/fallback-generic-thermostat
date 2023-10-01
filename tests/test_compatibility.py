0102b0ad5aded0e534d9518ca170da82d5a099d8
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-10-01 19:06:32 +0200

    Add tests

---
+++ tests/test_compatibility.py
@@ -0,0 +1,52 @@
+import hashlib
+import optparse
+import os
+from urllib.request import urlopen
+
+import pytest
+
+
+def version_hash(url: str) -> str:
+    remote = urlopen(url)
+    max_file_size = 100 * 1024 * 1024
+    h = hashlib.sha256()
+    total_read = 0
+    while True:
+        data = remote.read(4096)
+        total_read += 4096
+        if not data or total_read > max_file_size:
+            break
+        h.update(data)
+    return h.hexdigest()
+
+
+@pytest.fixture
+def versions() -> list[str]:
+    return [
+        "2023.8.4",
+        "2023.9.0",
+        "2023.9.1",
+        "2023.9.2",
+        "2023.9.3",
+    ]
+
+
+@pytest.fixture
+def shas(versions: list[str]) -> list[tuple[str, str]]:
+    return [
+        (
+            v,
+            version_hash(
+                f"https://raw.githubusercontent.com/home-assistant/core/{v}/homeassistant/components/generic_thermostat/climate.py"
+            ),
+        )
+        for v in versions
+    ]
+
+
+def test_compatibility(shas: list[tuple[str, str]]):
+    for version, sha in shas:
+        assert sha in [
+            "4aeb00650cdbf1d1aa526e84bbdf4ecf228d0f62341e6589fd97d37748273712",
+            "6f6b213280807f31643d0294aea857d959c17bf70c83159c62f6e439978eca54",
+        ], f"Version {version} not in sha list"
